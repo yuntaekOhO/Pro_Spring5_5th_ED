fa1bddf226719bab8ddf5cf93b0c257516cd83d3
yuntaekOhO <oyt1996@naver.com>
2022-01-18 19:44:52 +0900

    주석에 설명 추가하였음.

--- src/main/java/chapter03/DependencyPull.java
+++ src/main/java/chapter03/DependencyPull.java
@@ -7,7 +7,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 /*
     spring Application에서 의존성 풀을 사용하는 일반적인 예제 코드
     * 이런 종류의 IoC는 JNDI 룩업을 사용하는 EJB 2.1 이하의 버전에서만 자주 사용하는게 아닌
-    다양한 환경에서 스프링을 사용할 때도 꼭 필요하다.
+      다양한 환경에서 스프링을 사용할 때도 꼭 필요하다.
+    레지스트리에서 의존성을 가져온다.
  */
 public class DependencyPull {
     public static void main(String[] args) {
