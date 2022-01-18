44d5e2f87ec6aa62e65a70e54399b3668b4d8fd8
yuntaekOhO <oyt1996@naver.com>
2022-01-18 19:34:25 +0900

    Dependency Pull을 사용하는 일반적인 코드

---
+++ src/main/java/chapter03/DependencyPull.java
@@ -0,0 +1,18 @@
+package chapter03;
+
+import chapter02.decoupled.MessageRenderer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/*
+    spring Application에서 의존성 풀을 사용하는 일반적인 예제 코드
+    * 이런 종류의 IoC는 JNDI 룩업을 사용하는 EJB 2.1 이하의 버전에서만 자주 사용하는게 아닌
+    다양한 환경에서 스프링을 사용할 때도 꼭 필요하다.
+ */
+public class DependencyPull {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/app-context.xml");
+        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
+        mr.render();
+    }
+}
